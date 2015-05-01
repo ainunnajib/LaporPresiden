756e4bb117dca5ed5f06f19ed9209f5a50a5d4a3
aerios <aerismith.27@gmail.com>
2015-05-01 20:03:11 +0700

    Adding environment detection using  https://cloud.google.com/appengine/docs/php/cloud-sql/#PHP_Connect_post_and_get_from_your_database

--- Q2A-7/qa-config.php
+++ Q2A-7/qa-config.php
@@ -36,12 +36,24 @@
 	define('QA_MYSQL_DATABASE', 'your-mysql-db-name');
 */
 
-
+	if (isset($_SERVER['SERVER_SOFTWARE']) &&
+  		strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false){
+		define('QA_MYSQL_HOSTNAME', '/cloudsql/laporpresiden7:db');
+		define('QA_MYSQL_USERNAME', 'root');
+		define('QA_MYSQL_PASSWORD', 'R17081945i');
+		define('QA_MYSQL_DATABASE', 'laporpresiden');	
+	}else{
+		define('QA_MYSQL_HOSTNAME', '/var/run/mysqld/mysqld.sock');
+		define('QA_MYSQL_USERNAME', 'root');
+		define('QA_MYSQL_PASSWORD', 'nopassword');
+		define('QA_MYSQL_DATABASE', 'laporpresiden');	
+	}
 	
-	define('QA_MYSQL_HOSTNAME', '/cloudsql/laporpresiden7:db');
-	define('QA_MYSQL_USERNAME', 'root');
-	define('QA_MYSQL_PASSWORD', 'R17081945i');
-	define('QA_MYSQL_DATABASE', 'laporpresiden');
+	
+	// define('QA_MYSQL_HOSTNAME', '/var/run/mysqld/mysqld.sock');
+	// define('QA_MYSQL_USERNAME', 'root');
+	// define('QA_MYSQL_PASSWORD', 'nopassword');
+	// define('QA_MYSQL_DATABASE', 'laporpresiden');	
 
 /*
 	Ultra-concise installation instructions:

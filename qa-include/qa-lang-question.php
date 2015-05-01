56a41c063b286736a37d9633d319607e8374e0f8
aerios <aerismith.27@gmail.com>
2015-05-01 22:52:46 +0700

    update qa-lang-question.php in qa-include/

--- qa-include/qa-lang-question.php
+++ qa-include/qa-lang-question.php
@@ -1,6 +1,189 @@
 <?php
-/**
- * @deprecated This file is deprecated from Q2A 1.7; use the below file instead.
- */
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
 
-return (include 'lang/qa-lang-question.php');
+	File: qa-include/qa-lang-question.php
+	Description: Language phrases for question page
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_answer_title' => '1 Jawaban',
+		'a_convert_to_c_on' => 'Rubah jawaban ini menjadi komentar atas:',
+		'a_convert_to_c' => ' Rubah jawaban ini menjadi komentar',
+		'a_convert_warn_cs' => 'Warning: This conversion cannot be reversed and will also move this answer\'s comments.',
+		'a_convert_warn' => 'Warning: This conversion cannot be reversed.',
+		'a_notify_email' => 'Email me at this address if my answer is selected or commented on:',
+		'a_notify_label' => 'Email me if my answer is selected or commented on',
+		'a_notify_x_label' => 'Email me (^) if my answer is selected or commented on',
+		'a_waiting_your_approval' => 'Jawaban ini menunggu persetujuan anda',
+		'a_your_waiting_approval' => 'Jawaban anda akan di periksa dan disetujui dalam waktu cepat.',
+		'add_answer_button' => 'Tambahkan jawaban',
+		'add_comment_button' => 'Tambahkan komentar',
+		'add_q_favorites' => 'Jadikan Laporan in favorit',
+		
+		'answer_button' => 'Jawab',
+		'answer_limit' => 'Terlalu banyak jawaban masuk-coba lagi dalam waktu satu jam',
+		'answer_must_be_approved' => 'Akun anda harus disetujui sebelum anda dapat menjawab',
+		'answer_must_confirm' => 'Please ^5confirm your email address^6 to answer this question.',
+		'answer_must_login' => 'Please ^1log in^2 or ^3register^4 to answer this question.',
+		'answer_q_popup' => 'Jawab laporan ini',
+		'approve_a_popup' => 'Setujui jawaban ini',
+		'approve_button' => 'Setuju',
+		'approve_c_popup' => 'Setuju atas komentar ini',
+		'approve_q_popup' => 'Setuju atas laporan ini',
+
+		
+
+		'c_notify_email' => 'Email me at this address if a comment is added after mine:',
+		'c_notify_label' => 'Email me if a comment is added after mine',
+		'c_notify_x_label' => 'Email me (^) if a comment is added after mine',
+		'c_waiting_your_approval' => 'This comment is waiting for your approval',
+		'c_your_waiting_approval' => 'Your comment will be checked and approved shortly.',
+		'category_js_note' => 'To select any category, please enable Javascript in your web browser.',
+		'category_required' => 'Please choose a category',
+		'category_ask_not_allowed' => 'You do not have permission to ask questions in this category',
+		'claim_button' => 'I wrote this',
+		'claim_a_popup' => 'Assign this answer to your user account',
+		'claim_c_popup' => 'Assign this comment to your user account',
+		'claim_q_popup' => 'Assign this question to your user account',
+		'clear_flags_button' => 'clear flags',
+		'clear_flags_popup' => 'Remove flags by all users',
+		'close_button' => 'close',
+		'close_duplicate_error' => 'The duplicate question could not be found - please try entering the number from a different question URL, e.g. 123.',
+		'close_duplicate' => 'This is a duplicate of another question',
+		'close_form_button' => 'Tutup laporan',
+		'close_form_title' => 'Tutup laporan ini',
+		'close_original_note' => 'You can also enter the question number from the URL, e.g. 123.',
+		'close_original_title' => 'URL of the original question:',
+		'close_q_popup' => 'Close this question to any new answers',
+		'close_reason_title' => 'Alasan menutuo laporan ini:',
+		'closed_as_duplicate' => 'closed as a duplicate of:',
+		'closed_with_note' => 'closed with the note:',
+		'comment_a_popup' => 'Berikan komentar atas jawaban ini',
+		'comment_button' => 'Komentar',
+		'comment_limit' => 'Too many comments received - please try again in an hour',
+		'comment_must_be_approved' => 'Your account must be approved before you add a comment.',
+		'comment_must_confirm' => 'Please ^5confirm your email address^6 to add a comment.',
+		'comment_must_login' => 'Please ^1log in^2 or ^3register^4 to add a comment.',
+		'comment_on_a' => 'On answer: ',
+		'comment_on_q' => 'On question: ',
+		'comment_q_popup' => 'Berikan komentar atas laporan ini',
+		'delete_a_popup' => 'Hapus jawaban ini selamanya',
+		'delete_button' => 'Hapus',
+		'delete_c_popup' => 'Hapus komentar ini selamanya',
+		'delete_q_popup' => 'Hapus laporan ini selamanya',
+		'duplicate_content' => 'Your submission appears to be a duplicate.',
+		'edit_a_popup' => 'Edit jawaban ini',
+		'edit_a_title' => 'Edit jawaban',
+		'edit_button' => 'edit',
+		'edit_c_popup' => 'Edit komentar ini',
+		'edit_c_title' => 'Edit komentar',
+		'edit_must_confirm' => 'Please ^5confirm your email address^6 to edit this.',
+		'edit_must_login' => 'Please ^1log in^2 or ^3register^4 to edit this.',
+		'edit_q_popup' => 'Edit laporan',
+		'edit_q_title' => 'Edit laporan',
+		'example_tags' => 'contoh tags: ',
+		'flag_a_popup' => 'Flag this answer as spam or inappropriate',
+		'flag_button' => 'flag',
+		'flag_c_popup' => 'Flag this comment as spam or inappropriate',
+		'flag_hide_button' => 'flag and hide',
+		'flag_limit' => 'Too many posts flagged - please try again in an hour',
+		'flag_must_confirm' => 'Please ^5confirm your email address^6 to flag posts.',
+		'flag_must_login' => 'Please ^1log in^2 or ^3register^4 to flag posts.',
+		'flag_not_allowed' => 'Flagging this is not allowed',
+		'flag_q_popup' => 'Flag this question as spam or inappropriate',
+		'follow_a_popup' => 'Ask a new question relating to this answer',
+		'follow_button' => 'ask related question',
+		'follows_a' => 'related to an answer for:',
+		'follows_q' => 'Tentang laporan:',
+		'hide_a_popup' => 'sembunyikan jawaban ini',
+		'hide_button' => 'sembunyikan',
+		'hide_c_popup' => 'sembunyikan komentar ini',
+		'hide_q_popup' => 'sembunyikan laporan ini',
+		'matching_tags' => 'Matching tags: ',
+		'max_tags_x' => 'A maximum of ^ tags are allowed',
+		'min_tags_x' => 'Please provide at least ^ tag/s',
+
+		// Question language Start
+		'ask_button' => 'Laporkan',
+		'ask_follow_from_a' => 'Laporan Anda akan berhubungan dengan jawaban ini',
+		'ask_follow_title' => 'Ajukan laporan terkait',
+		'ask_limit' => 'Terlalu banyak laporan yang diterima - silakan coba lagi dalam satu jam',
+		'ask_must_be_approved' => 'Akun Anda harus disetujui sebelum Anda mengajukan pertanyaan.',
+		'ask_must_confirm' => 'Silakan konfirmasi alamat email Anda untuk mengajukan pertanyaan.',
+		'ask_must_login' => 'Silakan masuk atau daftar jika ingin memberikan laporan.',
+		'ask_same_q' => 'Sebelum melanjutkan cek pertanyaan anda',
+		'ask_title' => 'Berikan laporan',
+
+		'anon_name_label' => 'Nama yang akan di tampilkan (optional):',
+		'q_category_label' => 'Kategori',
+		'q_content_label' => 'Isi Laporan',
+		'q_hidden_author' => 'Laporan ini telah disembunyikan oleh penulisnya',
+		'q_hidden_flagged' => 'Laporan ini telah ditandai dan tersembunyi',
+		'q_hidden_other' => 'Laporan ini telah disembunyikan',
+		'q_notify_email' => 'Email saya di alamat ini jika pertanyaan saya dijawab atau dikomentari',
+		'q_notify_label' => 'Email saya jika pertanyaan saya dijawab atau dikomentari',
+		'notify_email_note' => 'Alamat email Anda hanya akan digunakan untuk mengirimkan pemberitahuan tersebut.',
+		'q_notify_x_label' => 'Email saya (^) jika pertanyaan saya dijawab atau dikomentari',
+		'q_tags_comma_label' => 'Tags - penggunaan tanda koma (,) sebagai pemisah',
+		'q_tags_label' => 'Tags - menggunakan tanda hubung untuk menggabungkan kata-kata',
+		'q_title_label' => 'Judul Laporan',
+		'q_waiting_approval' => 'Laporan ini menunggu persetujuan',
+		'q_waiting_your_approval' => 'Laporan ini menunggu persetujuan Anda',
+		'q_your_waiting_approval' => 'Laporan Anda akan diperiksa dan disetujui segera.',
+		// Question language End
+
+		'recat_button' => 'recategorize',
+		'recat_popup' => 'Change this question\'s category',
+		'recat_q_title' => 'Recategorize question',
+		'reject_a_popup' => 'Reject this answer',
+		'reject_button' => 'reject',
+		'reject_c_popup' => 'Reject this comment',
+		'reject_q_popup' => 'Reject this question',
+		'remove_q_favorites' => 'Remove this question from my favorites',
+		'reopen_button' => 'Buka kembali',
+		'reopen_q_popup' => 'Buka Kembali Laporan',
+		'reply_button' => 'Balas',
+		'reply_c_popup' => 'Balas komnentar ini',
+		'reshow_a_popup' => 'Tampilkan lagi jawaban ini',
+		'reshow_button' => 'Tampilkan lagi',
+		'reshow_c_popup' => 'Tampilkan lagi komentar ini',
+		'reshow_q_popup' => 'Tampilkan lagi laporan ini',
+		'retag_button' => 'retag',
+		'retag_cat_popup' => 'Ganti kategori atau tag laporan ini',
+		'retag_popup' => 'Ganti tag laporan ini',
+		'retag_q_title' => 'Retag Laporan',
+		'save_silent_label' => 'Save silently to hide that this was edited',
+		'select_popup' => 'Click to select as best answer',
+		'select_text' => 'Jawaban terbaik',
+		'show_1_comment' => 'Tampilkan 1 komentar',
+		'show_1_previous_comment' => 'Tampilkan satu komentar sebelumnya',
+		'show_x_comments' => 'Show ^ comments',
+		'show_x_previous_comments' => 'Show ^ previous comments',
+		'unflag_button' => 'unflag',
+		'unflag_popup' => 'Remove the flag that you added',
+		'unselect_popup' => 'Click to remove selection',
+		'x_answers_title' => '^ Jawaban',
+		'your_answer_title' => 'Jawaban Anda',
+		'your_comment_a' => 'Pendapat anda atas jawaban ini:',
+		'your_comment_q' => 'Pendapat anda atas pertanyaan ini:',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/

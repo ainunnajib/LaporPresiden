9c971befbeba60a59f882425111899aec9d9ec0f
aerios <aerismith.27@gmail.com>
2015-05-03 17:34:19 +0700

    update fb app id

--- qa-plugin/qa-share/qa-share-layer.php
+++ qa-plugin/qa-share/qa-share-layer.php
@@ -54,7 +54,7 @@
 					$this->output('<script type="text/javascript">(function() {var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;po.src = "https://apis.google.com/js/plusone.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);})();</script>');
 				}
 				if(qa_opt('share_plugin_facebook')) {
-					$this->output('<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=136209173223283";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>');
+					$this->output('<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=814638221947104";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));</script>');
 				}
 			}
 		}

06e1519ba16bde48c38d65f0484332a478a7e311
Andri Ma'ruf Anshori <and_drie69@yahoo.com>
2015-05-20 07:18:38 -0700

    update link footer di dev
    update link footer di dev untuk privasi dan kebijakan, tim lapor presiden

--- qa-theme/Classic/qa-theme.php
+++ qa-theme/Classic/qa-theme.php
@@ -228,6 +228,9 @@ qa_register_layer('/qa-admin-options.php', 'Theme Options', $theme_dir , $theme_
 							<div class="qa-attribution">
 								Oleh Tim Lapor Presiden. <a href="/feedback">Kirim Timbal Balik</a> |
 								Powered by <a href="http://www.question2answer.org/">Question2Answer</a>
+								<br />
+								<a href="/privasi-dan-kebijakan">Privasi Dan Kebijakan</a> |
+								<a href="/tim-lapor-presiden">Tim Lapor Presiden</a>
 							</div>
 							<div class="qa-footer-clear"> </div>
 						</div>');

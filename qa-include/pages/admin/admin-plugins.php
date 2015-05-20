d06fa4e676dc43330689a45aebdbec9c0402e50a
Andri Ma'ruf Anshori <and_drie69@yahoo.com>
2015-05-19 22:26:16 -0700

    plugin
    add plugin report

--- qa-include/pages/admin/admin-plugins.php
+++ qa-include/pages/admin/admin-plugins.php
@@ -84,7 +84,8 @@
 			QA_PLUGIN_DIR.'xml-sitemap/qa-plugin.php',
 			QA_PLUGIN_DIR.'q2a-xml-rpc-master/qa-plugin.php',
 			QA_PLUGIN_DIR.'qa-share/qa-plugin.php',
-			QA_PLUGIN_DIR.'q2a-post-merge-master/qa-plugin.php'
+			QA_PLUGIN_DIR.'q2a-post-merge-master/qa-plugin.php',
+			QA_PLUGIN_DIR.'laporpresiden-report/qa-plugin.php'
 		);
 
 	foreach ($moduletypes as $type) {
